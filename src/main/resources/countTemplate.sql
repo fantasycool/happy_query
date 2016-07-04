329e4d9039522eb4de58a59b6925fd539f740689
haofu <haofu@91jkys.com>
2016-07-04 16:14:25 +0800

    optimize

--- src/main/resources/countTemplate.sql
+++ src/main/resources/countTemplate.sql
@@ -1,26 +1,46 @@
-SELECT
-  count(*) as count_num
-from
-(
-  SELECT
-    ${primary_id}
-  FROM
-    ${left_table}
-  <#if left_operation_str ??>
-  where
-    ${left_operation_str}
-  </#if>
-) a
-${connect_type} join
-(
-	select
-      bb.left_id
+<#if only_right ??>
+  select
+     count(bb.left_id) as count_num
   from
     data_definition_value bb
 	where
 		${operation_str}
 	group by
 	  bb.left_id
-)b
-on
-	a.${primary_id}=b.left_id
+<#elseif only_left ??>
+    SELECT
+      count(*) as count_num
+    FROM
+      ${left_table}
+    <#if left_operation_str ??>
+    where
+      ${left_operation_str}
+    </#if>
+<#else>
+  SELECT
+    count(*) as count_num
+  from
+  (
+    SELECT
+      ${primary_id}
+    FROM
+      ${left_table}
+    <#if left_operation_str ??>
+    where
+      ${left_operation_str}
+    </#if>
+  ) a
+  ${connect_type} join
+  (
+    select
+        bb.left_id
+    from
+      data_definition_value bb
+    where
+      ${operation_str}
+    group by
+      bb.left_id
+  )b
+  on
+    a.${primary_id}=b.left_id
+</#if>
--- src/main/resources/queryTemplate.sql
+++ src/main/resources/queryTemplate.sql
@@ -1,39 +1,118 @@
-SELECT
-  a.*,
-  b.left_id,
-  b.int_strs,
-  b.varchar_strs,
-  b.double_strs,
-  b.feature_strs
-from
-(
+<#if only_right ??>
   SELECT
-    *
-  FROM
-    ${left_table}
-  <#if left_operation_str ??>
-  where
-    ${left_operation_str}
-  </#if>
-
-) a
-${connect_type} join
-(
-	select
-        aa.left_id as left_id,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.int_value) separator '|||') as int_strs,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.str_value) separator '|||') as varchar_strs,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.double_value) separator '|||') as double_strs,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.feature) separator '|||') as feature_strs
-  from
-    data_definition_value aa
-  INNER JOIN data_definition_value bb
-  on aa.left_id = bb.left_id
-	where
-		${operation_str}
-	group BY
-	  bb.left_id
-	limit ${start_index}, ${size}
-)b
-on
-	a.${primary_id}=b.left_id
+      a.*,
+      b.left_id,
+      b.int_strs,
+      b.varchar_strs,
+      b.double_strs,
+      b.feature_strs
+    from
+    (
+      SELECT
+        *
+      FROM
+        ${left_table}
+      <#if left_operation_str ??>
+      where
+        ${left_operation_str}
+      </#if>
+    ) a
+    right join
+    (
+      select
+            aa.left_id as left_id,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.int_value) separator '|||') as int_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.str_value) separator '|||') as varchar_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.double_value) separator '|||') as double_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.feature) separator '|||') as feature_strs
+      from
+        data_definition_value aa
+      INNER JOIN data_definition_value bb
+      on aa.left_id = bb.left_id
+      where
+        ${operation_str}
+      group BY
+        bb.left_id
+      limit ${start_index}, ${size}
+    )b
+    on
+      a.${primary_id}=b.left_id
+<#elseif only_left ??>
+    SELECT
+      a.*,
+      b.left_id,
+      b.int_strs,
+      b.varchar_strs,
+      b.double_strs,
+      b.feature_strs
+    from
+    (
+      SELECT
+        *
+      FROM
+        ${left_table}
+      <#if left_operation_str ??>
+      where
+        ${left_operation_str}
+      </#if>
+      limit ${start_index}, ${size}
+    ) a
+    left join
+    (
+      select
+            aa.left_id as left_id,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.int_value) separator '|||') as int_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.str_value) separator '|||') as varchar_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.double_value) separator '|||') as double_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.feature) separator '|||') as feature_strs
+      from
+        data_definition_value aa
+      INNER JOIN data_definition_value bb
+      on aa.left_id = bb.left_id
+      where
+        ${operation_str}
+      group BY
+        bb.left_id
+    )b
+    on
+      a.${primary_id}=b.left_id
+<#else>
+  SELECT
+      a.*,
+      b.left_id,
+      b.int_strs,
+      b.varchar_strs,
+      b.double_strs,
+      b.feature_strs
+    from
+    (
+      SELECT
+        *
+      FROM
+        ${left_table}
+      <#if left_operation_str ??>
+      where
+        ${left_operation_str}
+      </#if>
+    ) a
+    ${connect_type} join
+    (
+      select
+            aa.left_id as left_id,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.int_value) separator '|||') as int_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.str_value) separator '|||') as varchar_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.double_value) separator '|||') as double_strs,
+            group_concat(concat(aa.dd_ref_id, ':::', aa.feature) separator '|||') as feature_strs
+      from
+        data_definition_value aa
+      INNER JOIN data_definition_value bb
+      on aa.left_id = bb.left_id
+      where
+        ${operation_str}
+      group BY
+        bb.left_id
+    )b
+    on
+      a.${primary_id}=b.left_id
+    limit ${start_index}, ${size}
+</#if>
