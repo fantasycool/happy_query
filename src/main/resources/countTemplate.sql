1c8691b668a0c174030c7029527e0064f12f3780
haofu <haofu@91jkys.com>
2016-07-04 15:34:08 +0800

    add leftIds function

--- src/main/resources/countTemplate.sql
+++ src/main/resources/countTemplate.sql
@@ -3,28 +3,23 @@ SELECT
 from
 (
   SELECT
-    *
+    ${primary_id}
   FROM
     ${left_table}
   <#if left_operation_str ??>
   where
     ${left_operation_str}
   </#if>
-
 ) a
 ${connect_type} join
 (
 	select
-        bb.left_id as left_id,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.int_value) separator '|||') as int_strs,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.str_value) separator '|||') as varchar_strs,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.double_value) separator '|||') as double_strs,
-        group_concat(concat(aa.dd_ref_id, ':::', aa.feature) separator '|||') as feature_strs
+      bb.left_id
   from
     data_definition_value bb
 	where
 		${operation_str}
-	group BY
+	group by
 	  bb.left_id
 )b
 on
